0ab532570cf5f315ca1a374b8ec37a31b0e6a353
ilgnerbecheleni <ilgner.gui@gmail.com>
2023-10-02 18:22:00 -0300

    Adicionar arquivos de projeto.

---
+++ Modbus IP/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Modbus_IP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button5 = new System.Windows.Forms.Button();
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(137, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Conect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(218, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Disconect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(299, 68);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Read";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(137, 97);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Ler";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(455, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(455, 97);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(406, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Tensão";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(389, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Frequência";
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(137, 138);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Valores";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(810, 424);
+            this.chart1.TabIndex = 8;
+            this.chart1.Text = "chart1";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(218, 97);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Stop";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // txtIp
+            // 
+            this.txtIp.Location = new System.Drawing.Point(137, 42);
+            this.txtIp.Name = "txtIp";
+            this.txtIp.Size = new System.Drawing.Size(100, 20);
+            this.txtIp.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(114, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "IP";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(995, 710);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtIp);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox txtIp;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Modbus IP/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using NModbus;
+using NModbus.IO;
+using NModbus.Utility;
+using NModbus.Device;
+using System.Net.Http;
+using NModbus.Extensions.Enron;
+
+namespace Modbus_IP
+{
+    public partial class Form1 : Form
+    {
+        private IModbusMaster master;
+        private TcpClient tcpClient;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Endereço IP do dispositivo Modbus TCP
+                string ipAddress = "192.168.1.20";
+                int port = 502;
+
+                // Crie uma instância TcpClient para se conectar ao dispositivo Modbus
+                tcpClient = new TcpClient(ipAddress, port);
+
+                // Crie uma instância ModbusIpMaster para a comunicação
+                var factory = new ModbusFactory();
+                master = factory.CreateIpMaster(new TcpClientAdapter(tcpClient));
+
+                MessageBox.Show("Conexão bem-sucedida!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao conectar: " + ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Endereço do dispositivo Modbus IP
+                string ipAddress = "192.168.1.20"; // Substitua pelo endereço IP do seu dispositivo
+
+                // Porta do dispositivo Modbus IP (geralmente é 502)
+                int port = 502;
+
+                // Endereço do primeiro registro a ser lido
+                ushort startAddress = 55;
+
+                // Quantidade de registros a serem lidos
+                ushort numberOfRegisters = 2;
+
+                // Crie uma instância TcpClient para se conectar ao dispositivo Modbus IP
+                using (TcpClient tcpClient = new TcpClient(ipAddress, port))
+                {
+                    // Crie uma instância ModbusIpMaster para a comunicação
+                    ModbusFactory factory = new ModbusFactory();
+                    IModbusMaster master = factory.CreateMaster(tcpClient);
+
+                    // Faça uma leitura de registrador
+                    ushort[] data = master.ReadHoldingRegisters((byte)1, startAddress, numberOfRegisters);
+
+                    // Exiba os valores lidos em formato hexadecimal
+                    string result = "Valores lidos (hex): ";
+                    for (int i = 0; i < data.Length; i++)
+                    {
+                        result += data[i].ToString("X4") + " ";
+                    }
+
+                    // Chame a função de conversão para obter os valores reais
+                    double valorReal1 = ConvertToRealValue(data[0], data[1]);
+
+                    // Exiba os valores reais
+                    result += "\nValor Real 1: " + valorReal1 + "Volts";
+
+                    // Exiba o resultado
+                    MessageBox.Show(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao ler: " + ex.Message);
+            }
+
+        }
+
+
+        public double ReadRegisterValue(string ipAddress, ushort startAddress)
+        {
+            try
+            {
+                // Porta do dispositivo Modbus IP (geralmente é 502)
+                int port = 502;
+
+                // Quantidade de registros a serem lidos
+                ushort numberOfRegisters = 2;
+
+                // Crie uma instância TcpClient para se conectar ao dispositivo Modbus IP
+                using (TcpClient tcpClient = new TcpClient(ipAddress, port))
+                {
+                    // Crie uma instância ModbusIpMaster para a comunicação
+                    ModbusFactory factory = new ModbusFactory();
+                    IModbusMaster master = factory.CreateMaster(tcpClient);
+
+                    // Faça uma leitura de registrador
+                    ushort[] data = master.ReadHoldingRegisters((byte)1, startAddress, numberOfRegisters);
+
+                    // Chame a função de conversão para obter os valores reais
+                    double valorReal1 = ConvertToRealValue(data[0], data[1]);
+
+                    return valorReal1;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao ler: " + ex.Message);
+                return 0.0; // Ou outro valor de retorno apropriado em caso de erro
+            }
+        }
+
+        public static double ConvertToRealValue(ushort highRegister, ushort lowRegister)
+        {
+            // Junte os registradores que compõem a grandeza
+            int combinedValue = (highRegister << 16) | lowRegister;
+
+            // Extraia o bit de sinal
+            bool isNegative = (combinedValue & 0x80000000) != 0;
+
+            // Extraia os 8 bits que indicam a parte inteira
+            int intPartBits = (combinedValue >> 23) & 0xFF;
+
+            // Extraia os bits restantes (parte fracionária)
+            int fracPartBits = combinedValue & 0x7FFFFF;
+
+            // Calcule o valor real
+            double realValue = 0.0;
+
+            if (intPartBits != 0)
+            {
+                realValue = (isNegative ? -1 : 1) * (1 + (double)fracPartBits / Math.Pow(2, 23)) * Math.Pow(2, intPartBits - 127);
+            }
+
+            return realValue;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Feche a conexão com o dispositivo Modbus
+            if (tcpClient != null)
+            {
+                tcpClient.Close();
+                tcpClient = null;
+                master.Dispose();
+                master = null;
+                MessageBox.Show("Desconectado com sucesso!");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //string ipAddress = "192.168.1.20";
+            string ipAddress = txtIp.Text ;
+            Console.WriteLine(ipAddress);
+            ushort startAddress = 55;
+            double valorLido = ReadRegisterValue(ipAddress, startAddress);
+            MessageBox.Show("Valor Real: " + valorLido + " Volts");
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string ipAddress = "192.168.1.20"; // Substitua pelo endereço IP do seu dispositivo
+
+            // Ler o registrador 1
+            double valorRegistrador1 = ReadRegisterValue(ipAddress, 1);
+
+            // Ler o registrador 55
+            double valorRegistrador55 = ReadRegisterValue(ipAddress, 55);
+
+            // Exibir os valores nos TextBoxes
+            textBox1.Text =  valorRegistrador1.ToString("F2") + " V";
+            textBox2.Text =  valorRegistrador55.ToString("F2") + " Hz";
+            int MaxDataPoints = 10;
+            // Adicione o valor lido como um novo ponto no gráfico
+            chart1.Series["Valores"].Points.AddY(valorRegistrador1);
+
+            // Atualize o gráfico
+            chart1.Update();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+    }
+}
